ef73368f5837458a4285b7fdbfb35e3873c31e65
KameguchiYu <yu.kameguchi8010@gmail.com>
2023-11-10 10:53:09 +0900

    プロジェクト ファイルを追加します。

---
+++ 10121/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int money = 0;
+	cout << "今年もらったお年玉の金額はいくらですか？" << flush;
+	cin >> money;
+	cout << "あなたが今年貰ったお年玉の金額は" << "\\" << money << "です。" << endl;
+
+}
